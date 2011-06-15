75a9f133688986060f51d2615c1118fbc440b8c4
Ralph Bean <ralph.bean@gmail.com>
2011-06-15 15:01:57 -0400

    PEP8 compliance and new mako_utils.compat function.

--- tw2/core/mako_util.py
+++ tw2/core/mako_util.py
@@ -11,6 +11,7 @@ _BOOLEAN_ATTRS = frozenset(['selected', 'checked', 'compact', 'declare',
                             'defer', 'disabled', 'ismap', 'multiple',
                             'nohref', 'noresize', 'noshade', 'nowrap'])
 
+
 def attrs(context, args=None, attrs=None):
     # Emulates Genshi's AttrsDirective (poorly)
     if isinstance(args, list):
@@ -24,5 +25,36 @@ def attrs(context, args=None, attrs=None):
     bools = _BOOLEAN_ATTRS
 
     new_attrs = [u'%s="%s"' % (k, escape(unicode(k in bools and k or v), True))
-                      for k,v in args.iteritems() if (k not in bools and v is not None) or (k in bools and v)]
+                 for k, v in args.iteritems()
+                 if (k not in bools and v is not None) or (k in bools and v)]
     return u" ".join(new_attrs)
+
+
+def compat(context, attr):
+    """ Backwards compatible widget attribute access.
+
+    In tw1, all template attributes looked like:
+        ${some_attribute}
+
+    Whereas in tw2 they look like:
+        ${w.some_attribute}
+
+    This is a *nuisance* if you want to reuse a template between tw1 and tw2
+    widgets.  With this function you can write:
+        <%namespace name="tw" module="tw2.core.mako_util"/>
+        ${tw.compat(attr='some_attribute')}
+    or
+        ${tw._('some_attribute')}
+
+    Nice, right? :)
+    """
+
+    if not 'w' in context.keys():
+        # Must be tw1
+        return context.get(attr)
+    else:
+        # Must be tw2
+        return getattr(context.get('w'), attr)
+
+
+_ = compat  # Just for shorthand
