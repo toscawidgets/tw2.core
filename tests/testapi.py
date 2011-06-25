d4c560f9b7f0fdd3626e98c2286a992e100f1a11
Tom Willis <tom@batterii.com>
2011-06-25 08:51:21 -0400

    while trying to calculate coverage I discovered that a few tests would blow up if nose was run this way....
    ../../bin/nosetests --with-coverage --cover-package=tw2.core -v -w
    tests
    
    
    ======================================================================
    ERROR: tests.test_hierarchy.TestHierarchy.test_display_only
    ----------------------------------------------------------------------
    Traceback (most recent call last):
    ....
      "/Users/twillis/projects/tosca_dev/src/tw2.core/tests/testapi.py",
      line 27, in request
        rl = twc.core.request_local()
      File
      "/Users/twillis/projects/tosca_dev/src/tw2.core/tw2/core/testbase/base.py",
      line 68, in request_local_tst
        return _request_local[_request_id]
    TypeError: 'NoneType' object is not subscriptable
    
    ======================================================================
    ERROR: tests.test_hierarchy.TestHierarchy.test_rw_propagate
    ----------------------------------------------------------------------
    Traceback (most recent call last):
    ....
      "/Users/twillis/projects/tosca_dev/src/tw2.core/tests/testapi.py",
      line 27, in request
        rl = twc.core.request_local()
      File
      "/Users/twillis/projects/tosca_dev/src/tw2.core/tw2/core/testbase/base.py",
      line 68, in request_local_tst
        return _request_local[_request_id]
    TypeError: 'NoneType' object is not subscriptable
    
    ======================================================================
    ERROR: tests.test_hierarchy.TestHierarchy.test_rwb
    ----------------------------------------------------------------------
    Traceback (most recent call last):
    ....
      "/Users/twillis/projects/tosca_dev/src/tw2.core/tests/testapi.py",
      line 27, in request
        rl = twc.core.request_local()
      File
      "/Users/twillis/projects/tosca_dev/src/tw2.core/tw2/core/testbase/base.py",
      line 68, in request_local_tst
        return _request_local[_request_id]
    TypeError: 'NoneType' object is not subscriptable
    
    
    Which led me to discover that testapi.py was trying to do the same
    thing as tw2.core.testbase.base so I changed testapi.py to just use
    those functions out of tw2.core.testbase.base

--- tests/testapi.py
+++ tests/testapi.py
@@ -1,30 +1,19 @@
 import tw2.core as twc
+from tw2.core.testbase import base
 from tw2.core.template import reset_engine_name_cache
 
-def request_local_tst():
-#    if _request_id is None:
-#        raise KeyError('must be in a request')
-    try:
-        return _request_local[_request_id]
-    except KeyError:
-        rl_data = {}
-        _request_local[_request_id] = rl_data
-        return rl_data
+twc.core.request_local = base.request_local_tst
 
-twc.core.request_local = request_local_tst
-_request_local = {}
 
 def setup():
-    global _request_local, _request_id
-    _request_local = {}
-    _request_id = None
+    base._request_local = {}
+    base._request_id = None
     reset_engine_name_cache()
 
 
 def request(requestid, mw=None):
-    global _request_id
-    _request_id = requestid
+    base._request_id = requestid
     rl = twc.core.request_local()
     rl.clear()
     rl['middleware'] = mw
-    return request_local_tst()
+    return base.request_local_tst()
