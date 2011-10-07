bc5c8860e6555ddf1a072e9a9089da891f2f011f
Ralph Bean <ralph.bean@gmail.com>
2011-10-07 11:26:31 -0400

    New version containing rodfersou's bugfix

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ _extra_mako = ["Mako >= 0.1.1"]
 
 setup(
     name='tw2.core',
-    version='2.0b6',
+    version='2.0b7',
     description="Web widget creation toolkit based on TurboGears widgets",
     long_description = get_description(),
     install_requires=[
