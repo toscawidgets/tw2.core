cccb8ddee4dfc55b4e578e9f3d591280ac1033ba
Nils Philippsen <nils@tiptoe.de>
2018-05-15 10:52:02 +0200

    test that the best available translation is used

--- tests/test_i18n.py
+++ tests/test_i18n.py
@@ -1,3 +1,9 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from nose.tools import eq_
+
 import testapi
 import tw2.core as twc
 
@@ -9,3 +15,16 @@ class TestI18N(object):
     def test_get_translator(self):
         # Test that get_translator() doesn't throw an exception.
         twc.i18n.get_translator()
+
+    def test_primary_translation(self):
+        # Test that the best possible translation is used.
+        translator = twc.i18n.get_translator(lang=('de', 'en'))
+        eq_(translator("Must be a valid URL"),
+            "Bitte eine gültige URL eingeben")
+
+    def test_skip_unknown_language(self):
+        # Test that languages without a corresponding message catalog are
+        # skipped.
+        translator = twc.i18n.get_translator(lang=('nolanguage', 'de', 'en'))
+        eq_(translator("Must be a valid URL"),
+            "Bitte eine gültige URL eingeben")
