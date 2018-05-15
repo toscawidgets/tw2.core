82d49bb9158dbd1f610e90954de1b54372361e41
Nils Philippsen <nils@tiptoe.de>
2018-05-15 11:15:46 +0200

    ensure that translated test message won't vanish
    Marks the test message that is to be translated as translatable and
    extract it from the source file.

--- tests/test_i18n.py
+++ tests/test_i18n.py
@@ -5,7 +5,11 @@ from __future__ import unicode_literals
 from nose.tools import eq_
 
 import testapi
+
 import tw2.core as twc
+
+
+def _(x): return x
 
 
 class TestI18N(object):
@@ -19,12 +23,12 @@ class TestI18N(object):
     def test_primary_translation(self):
         # Test that the best possible translation is used.
         translator = twc.i18n.get_translator(lang=('de', 'en'))
-        eq_(translator("Must be a valid URL"),
+        eq_(translator(_("Must be a valid URL")),
             "Bitte eine gültige URL eingeben")
 
     def test_skip_unknown_language(self):
         # Test that languages without a corresponding message catalog are
         # skipped.
         translator = twc.i18n.get_translator(lang=('nolanguage', 'de', 'en'))
-        eq_(translator("Must be a valid URL"),
+        eq_(translator(_("Must be a valid URL")),
             "Bitte eine gültige URL eingeben")
